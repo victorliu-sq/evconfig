516a3d54a6a52894e8336c5f7c805580f91ecf41
jiaxinliu <jiaxin_liu@brown.edu>
2024-01-30 19:45:08 -0500

    first

---
+++ elpa/evil-20240123.1601/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20240123.1601" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "9e27bb9e5a66cacbf9f1f4f20ca9b30fc2ae297b" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
